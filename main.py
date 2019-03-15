e6134350b2da55ade9bfede16a028be4cd951192
renard162 <renard162@users.noreply.github.com>
2019-03-14 23:00:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,21 +28,30 @@ theta=model1.last_squares(psi,y)
 #print(reg_code_)
 #print(w[0:10,0:4])
 
-test = sys_identfy.autocorr(y_ident_)
+# test = sys_identfy.autocorr(y_ident_)
 # plt.plot(teste)
 # plt.show()
-print(np.max(test))
+# print(np.max(test))
 
-#yy=psi@theta
+yy=psi@theta
 #print(theta)
 print(model)
-print(errr)
+# print(errr)
 print(pivv)
-print(theta)
+# print(theta)
 #================================================
 
+# a0 = pivv[0:len(model)]
+# a = np.copy(w[:, a0])
+# b = a @ theta
+c = model1.model_prediction(model,y,u,theta)
 
+# print(len(u)-model1.max_lag)
+# print(len(c))
 
+# plt.plot(c)
+# plt.plot(y_ident_)
+# plt.show()
 
 # y_livre = y[0:model1.max_lag]
 # for i in range((model1.max_lag),u.size):
@@ -55,10 +64,8 @@ print(theta)
 # for i in range((model1.max_lag),u.size):
 #     y_livre[i]=theta[0]*y_livre[i-1]+theta[1]*y_livre[i-2]+theta[2]*u[i-1]+theta[3]*y_livre[i-1]*u[i-1]+theta[4]*u[i-2]+theta[5]*y_livre[i-2]*u[i-1]+theta[6]*y_livre[i-1]*u[i-2]+theta[7]*y_livre[i-2]*u[i-2]+theta[8]*y_livre[i-1]*y_livre[i-1]+theta[9]*y_livre[i-2]*y_livre[i-2]
 # print(y_livre)
+# plt.plot(y)
 # plt.plot(y_livre)
 # plt.show()
 
-
-
-
-
+print('fim')
