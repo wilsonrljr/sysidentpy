e41a671e09faab83e6b53e234c290b748ff31514
wilsonrljr <wilsonrljr@outlook.com>
2019-07-08 12:05:44 -0300

    Added README.rst and update setup.py

---
+++ README.rst
@@ -0,0 +1,92 @@
+.. -*- mode: rst -*-
+
+sys-identpy
+============
+
+sys-identpy is a Python module for System Identification and
+machine learning using NARX models built on top of numpy and
+is distributed under the 3-Clause BSD license.
+
+The project was started in 2018 by Wilson R. L. Junior, Luan
+Pascoal and Samir A. M. Martins as a project for System Identification
+discipline. Samuel joined early in 2019 and since then have contributed.
+
+Website: http://sys-identpy.org
+
+Installation
+------------
+
+Dependencies
+~~~~~~~~~~~~
+
+sys-identpy requires:
+
+- Python (>= 3.5)
+- NumPy (>= 1.1.0)
+- SciPy (>= 1.3.0)
+- itertools (>= 2.5)
+
+**Sys-identpy do not to support Python 2.7.**
+sys-identpy Python 3.5 or newer.
+
+For running the examples Matplotlib >= 1.5.2 is required. A
+few examples require pandas >= 0.18.0.
+
+User installation
+~~~~~~~~~~~~~~~~~
+
+If you already have a working installation of numpy and scipy,
+the easiest way to install sys-identpy is using ``pip``   ::
+
+    pip install -U sys-identpy
+
+or ``conda``::
+
+    conda install sys-identpy
+
+Changelog
+---------
+
+See the `changelog <http://sys-identpy.org/dev/whats_new.html>`__
+for a history of notable changes to sys-identpy.
+
+Development
+-----------
+
+We welcome new contributors of all experience levels. The sys-identpy
+community goals are to be helpful, welcoming, and effective.
+
+Important links
+~~~~~~~~~~~~~~~
+
+- Official source code repo: https://github.com/wilsonrljr/sys-identpy
+- Download releases: https://pypi.org/project/sys-identpy/
+
+Source code
+~~~~~~~~~~~
+
+You can check the latest sources with the command::
+
+    git clone https://github.com/wilsonrljr/sys-identpy.git
+
+Project History
+---------------
+
+The project was started in 2018 by Wilson R. L. Junior, Luan
+Pascoal and Samir A. M. Martins as a project for System Identification
+discipline. Samuel joined early in 2019 and since then have contributed.
+
+The project is currently maintained by its creators and looking for
+contributors.
+
+Communication
+~~~~~~~~~~~~~
+
+- Mailing list: https://sys-identpy.org/contact
+- IRC channel: ``@sys-identpy`` at ``web.telegram.org``
+- Website: http://sys-identpy.org
+
+Citation
+~~~~~~~~
+
+If you use sys-identpy in a scientific publication, we would appreciate citations: http://sys-identpy.org/stable/about.html#citing-sys-identpy
--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
+"""SysIdentPy: System Identification Library for Python
+is open-source software for mathematics,
+science, and engineering regarding the use of NARX models
+built on top of numpy and is distributed under the 3-Clause BSD license.
+
+"""
+
+import os
+import sys
+import subprocess
+import textwrap
+import warnings
+import sysconfig
+from distutils.version import LooseVersion
 from __future__ import print_function
 import sys
 from setuptools import setup, find_packages
+import builtins
+import platform
+
+
+
+if sys.version_info[:2] < (3, 5):
+    raise RuntimeError("Python version >= 3.5 required.")
 
 with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
@@ -18,22 +39,65 @@ except ImportError:
     print('itertools is required during installation')
     sys.exit(1)
 
+DISTNAME = 'sys-identpy'
+DESCRIPTION = 'Open source system identification library in Python'
+LONG_DESCRIPTION = 'Take a look into https://github.com/wilsonrljr'
+MAINTAINER = 'Wilson Rocha Lacerda Junior'
+MAINTAINER_EMAIL = 'wilsonrljr@outlook.com'
+URL = 'http://sys-identpy.org'
+DOWNLOAD_URL = 'https://pypi.org/project/sys-identpy/#files'
+LICENSE = 'new BSD'
+PROJECT_URLS = {
+    'Source Code': 'https://github.com/wilsonrljr/sys-identpy'
+}
+
 import sys_identfy
 
-setup(name='sys_identify',
-      version='0.5',
-      description='Open source system identification library in Python',
-      long_description=" Take a look into https://github.com/wilsonrljr",
-      author='Wilson Luan Samuel Samir',
-      packages=find_packages(),
-      install_requires=INSTALL_REQUIRES,
-      author_email='wilsonrljr@outlook.com',
-      url='https://github.com/wilsonrljr/sys_identify',
-      classifiers=[
-              'Programming Language :: Python :: 3.6',
-              'Development Status :: 2 - Beta',
-              'Intended Audience :: Science/Research',
-              'Topic :: Scientific/Engineering :: System Identification',
-              'Topic :: Software Development :: Libraries'
-              ]
-      )
+VERSION = sys_identfy.__version__
+
+if platform.python_implementation() == 'PyPy':
+    ITERTOOLS_MIN_VERSION = '2.6'
+    NUMPY_MIN_VERSION = '1.14.0'
+else:
+    ITERTOOLS_MIN_VERSION = '2.6'
+    NUMPY_MIN_VERSION = '1.11.0'
+
+
+def setup_package():
+    metadata = dict(name=DISTNAME,
+                    maintainer=MAINTAINER,
+                    maintainer_email=MAINTAINER_EMAIL,
+                    description=DESCRIPTION,
+                    license=LICENSE,
+                    url=URL,
+                    download_url=DOWNLOAD_URL,
+                    project_urls=PROJECT_URLS,
+                    version=VERSION,
+                    long_description=LONG_DESCRIPTION,
+                    classifiers=[
+                            'Intended Audience :: Science/Research',
+                            'Intended Audience :: Developers',
+                            'Programming Language :: Python :: 3.5',
+                            'Programming Language :: Python :: 3.6',
+                            'Programming Language :: Python :: 3.7',
+                            'Development Status :: 3 - Alpha',
+                            'Intended Audience :: Science/Research',
+                            'Topic :: Scientific/Engineering',
+                            'Topic :: Software Development'
+                            'Operating System :: Microsoft :: Windows',
+                            'Operating System :: POSIX',
+                            'Operating System :: Unix',
+                            'Operating System :: MacOS',
+                            ],
+                    python_requires=">=3.5",
+                    install_requires=[
+                                        'numpy>={}'.format(NUMPY_MIN_VERSION),
+                                        'itertools>={}'.format(ITERTOOLS_MIN_VERSION)
+                                    ],
+                    )
+
+    setup(**metadata)
+
+
+if __name__ == "__main__":
+    setup_package()
