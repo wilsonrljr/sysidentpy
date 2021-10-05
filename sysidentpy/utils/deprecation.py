032574208be22d5554970b0f569118024781a930
Wilson Rocha <wilsonrljr@outlook.com>
2021-10-05 20:26:54 -0300

    update the methods in deprecation.py. The method used is adapted from astroML (code taken from their github)

--- sysidentpy/utils/deprecation.py
+++ sysidentpy/utils/deprecation.py
@@ -2,10 +2,7 @@
 #           Wilson Rocha Lacerda Junior <wilsonrljr@outlook.com>
 # License: BSD 3 clause
 
-
-from functools import wraps
 import warnings
-import inspect
 import functools
 
 
@@ -54,70 +51,3 @@ def deprecated(version, future_version=None, message=None, alternative=None, **k
             return deprecate_function(obj)
 
     return deprecate_warning
-
-def deprecated_method(since, message='', alternative=None, **kwargs):
-        def deprecate_function(func, message=message, since=since,
-                               alternative=alternative):
-            if message == '':
-                message = ('Function {} has been deprecated since {}.'
-                           .format(func.__name__, since))
-                if alternative is not None:
-                    message += '\n Use {} instead.'.format(alternative)
-
-            @functools.wraps(func)
-            def deprecated_func(*args, **kwargs):
-                warnings.warn(message, FutureWarning)
-                return func(*args, **kwargs)
-            return deprecated_func
-
-        def deprecate_class(cls, message=message, since=since,
-                            alternative=alternative):
-            if message == '':
-                message = ('Class {} has been deprecated since {}.'
-                           .format(cls.__name__, since))
-                if alternative is not None:
-                    message += '\n Use {} instead.'.format(alternative)
-
-            cls.__init__ = deprecate_function(cls.__init__, message=message)
-
-            return cls
-
-        def deprecate(obj):
-            if isinstance(obj, type):
-                return deprecate_class(obj)
-            else:
-                return deprecate_function(obj)
-
-        return deprecate
-
-
-def _deprecate_positional_args(func):
-    """Decorator to mark a function or class as deprecated.
-    """
-    # @wraps(func)
-    def wrapper(*args, **kwargs):
-        # func(*args, **kwargs)
-        warnings.warn(
-                f"Pass arguments as keyword args. From version "
-                f"v0.2.0 passing these as positional arguments "
-                "will result in an error",
-                FutureWarning,
-            )
-        func()
-
-    return wrapper
-
-def _deprecate_polynomial_narmax(func):
-    """Decorator to mark a function or class as deprecated.
-    """
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        func(*args, **kwargs)
-        warnings.warn(
-                "This module was deprecated in version 0.2 in favor of "
-                "the model_structure_selection.FROLS module into which all the refactored "
-                "classes and functions are moved.",
-                FutureWarning,
-            )
-
-    return wrapper
