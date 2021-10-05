109771a2f4b463a476201040b755b206eb5457d4
Wilson Rocha <wilsonrljr@outlook.com>
2021-10-05 20:58:11 -0300

    add new GenerateRegressors class. Now this class is adapted to handle NARMAX, NAR and NFIR models

--- sysidentpy/narmax_base.py
+++ sysidentpy/narmax_base.py
@@ -32,3 +32,126 @@ def _get_max_lag(ylag=1, xlag=1):
     return np.max([ny, np.max(nx)])
 
 
+class GenerateRegressors:
+    """Polynomial NARMAX model
+
+    Provides the main functions to generate the regressor dictionary
+    and regressor codes for polynomial basis.
+    """
+
+    def create_narmax_code(self, non_degree, xlag, ylag, n_inputs):
+        """Create the code representation of the regressors.
+
+        This function generates a codification from all possibles
+        regressors given the maximum lag of the input and output.
+        This is used to write the final terms of the model in a
+        readable form. [1001] -> y(k-1).
+        This code format was based on a dissertation from UFMG. See
+        reference below.
+
+        Parameters
+        ----------
+        non_degree : int
+            The desired maximum nonlinearity degree.
+        ylag : int
+            The maximum lag of output regressors.
+        xlag : int
+            The maximum lag of input regressors.
+
+        Returns
+        -------
+        max_lag : int
+            This value can be used by another functions.
+        regressor_code : ndarray of int
+            Matrix codification of all possible regressors.
+
+        Examples
+        --------
+        The codification is defined as:
+
+        >>> 100n = y(k-n)
+        >>> 200n = u(k-n)
+        >>> [100n 100n] = y(k-n)y(k-n)
+        >>> [200n 200n] = u(k-n)u(k-n)
+
+        References
+        ----------
+        [1] Master Thesis: Barbosa, Alípio Monteiro.
+            Técnicas de otimização bi-objetivo para a determinação
+            da estrutura de modelos NARX (2010).
+
+        """
+        if not isinstance(non_degree, int) or non_degree < 1:
+            raise ValueError(
+                "non_degree must be integer and > zero. Got %f" % non_degree
+            )
+
+        if not isinstance(ylag, (int, list)) or np.min(np.minimum(ylag, 1)) < 1:
+            raise ValueError("ylag must be integer or list and > zero. Got %f" % ylag)
+
+        if (
+            not isinstance(xlag, (int, list))
+            # or np.min(np.minimum(xlag, 1)) < 1):
+            or np.min(np.min(list(chain.from_iterable([[xlag]])))) < 1
+        ):
+            raise ValueError("xlag must be integer or list and > zero. Got %f" % xlag)
+
+        if not isinstance(n_inputs, int) or n_inputs < 1:
+            raise ValueError("n_inputs must be integer and > zero. Got %f" % n_inputs)
+
+        if isinstance(ylag, list):
+            # create only the lags passed from list
+            y_vec = []
+            y_vec.extend([lag + 1000 for lag in ylag])
+            y_vec = np.array(y_vec)
+        else:
+            # create a range of lags if passed a int value
+            y_vec = np.arange(1001, 1001 + ylag)
+
+        if isinstance(xlag, list) and n_inputs == 1:
+            # create only the lags passed from list
+            x_vec_tmp = []
+            x_vec_tmp.extend([lag + 2000 for lag in xlag])
+            x_vec_tmp = np.array(x_vec_tmp)
+        elif isinstance(xlag, int) and n_inputs == 1:
+            # create a range of lags if passed a int value
+            x_vec_tmp = np.arange(2001, 2001 + xlag)
+        elif n_inputs > 1:
+            # only list are allowed if n_inputs > 1
+            # the user must entered list of the desired lags explicitly
+            x_vec_tmp = []
+            for i in range(n_inputs):
+                if isinstance(xlag[i], list) and n_inputs > 1:
+                    # create 200n, 300n,..., 400n to describe each input
+                    x_vec_tmp.extend([lag + 2000 + i * 1000 for lag in xlag[i]])
+                elif isinstance(xlag[i], int) and n_inputs > 1:
+                    x_vec_tmp.extend(
+                        [np.arange(2001 + i * 1000, 2001 + i * 1000 + xlag[i])]
+                    )
+
+        if n_inputs > 1:
+            # if x_vec is a nested list, ensure all elements are arrays
+            all_arrays = [np.array([i]) if isinstance(i, int) else i for i in x_vec_tmp]
+            x_vec = np.concatenate([i for i in all_arrays])
+        else:
+            x_vec = x_vec_tmp
+
+        return x_vec, y_vec
+    
+    def regressor_space(self, non_degree, xlag, ylag, n_inputs, model_type="NARMAX"):
+        x_vec, y_vec = self.create_narmax_code(non_degree, xlag, ylag, n_inputs)
+        reg_aux = np.array([0])
+        if model_type == "NARMAX":
+            reg_aux = np.concatenate([reg_aux, y_vec, x_vec])
+        elif model_type == "NAR":
+            reg_aux = np.concatenate([reg_aux, y_vec])
+        elif model_type == "NFIR":
+            reg_aux = np.concatenate([reg_aux, x_vec])
+        else:
+            raise Exception("Unrecognized model type. Model type should be NARMAX, NAR or NFIR")
+
+        regressor_code = list(combinations_with_replacement(reg_aux, non_degree))
+
+        regressor_code = np.array(regressor_code)
+        regressor_code = regressor_code[:, regressor_code.shape[1] :: -1]
+        return regressor_code
