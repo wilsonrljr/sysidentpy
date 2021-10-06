100d61f43f2ed04115d78bfa5ca6560f4158f5ce
Wilson Rocha <wilsonrljr@outlook.com>
2021-10-05 22:55:20 -0300

    Add methods to compute autocorrelation and cross correlation in residual analysis. Now the methods are splited and each output are computed separately. Besides the efficiency, now its not limited by two inputs and will be easier to expand

--- sysidentpy/residues/residues_correlation.py
+++ sysidentpy/residues/residues_correlation.py
@@ -10,6 +10,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def get_residues_autocorrelation(y, yhat):
+    e = calculate_residues(y, yhat)
+    unnormalized_e_acf = get_unnormalized_e_acf(e)
+    half_of_symmetry_autocorr = int(np.floor(unnormalized_e_acf.size / 2))
+
+    e_acf = (
+        unnormalized_e_acf[half_of_symmetry_autocorr:]
+        / unnormalized_e_acf[half_of_symmetry_autocorr]
+    )
+    
+    half_of_symmetry_autocorr = int(np.floor(unnormalized_e_acf.size / 2))
+    n = len(unnormalized_e_acf) - half_of_symmetry_autocorr
+    upper_bound = np.ones(n) * (
+        1.96 / np.sqrt(len(unnormalized_e_acf))
+    )
+    lower_bound = upper_bound * (-1)
+    return e_acf, upper_bound, lower_bound
+
+def calculate_residues(y, yhat):
+    return (y - yhat).flatten()
+
+def get_unnormalized_e_acf(e):
+    return np.correlate(e, e, mode="full")
+
+def compute_cross_correlation(y, yhat, arr):
+    e = calculate_residues(y, yhat)
+    n = len(e) * 2 - 1
+    ccf, upper_bound, lower_bound = _input_ccf(e, arr, n)
+    return ccf, upper_bound, lower_bound    
+    
+def _input_ccf(e, a, n):
+    ccf = _normalized_correlation(a, e)
+    upper_bound = np.ones(len(ccf[:, 0])) * (1.96 / np.sqrt(n))
+    lower_bound = upper_bound * (-1)
+    return ccf, upper_bound, lower_bound
+
+def _normalized_correlation(a, b):
+    """Compute the normalized correlation between two signals.
+
+    Parameters
+    ----------
+    a : array-like of shape = n_samples.
+    b : array-like of shape = n_samples.
+
+    Returns
+    -------
+    ruy : ndarray of floats:
+        The normalized cross correlation between the two signals.
+
+    """
+    y = (a - np.mean(a)).flatten()
+    u = (b - np.mean(b)).flatten()
+    t = int(np.floor(len(a) / 2))
+    ruy = np.array(np.zeros(t))
+    ruy[0] = np.sum(y * u) / (np.sqrt(np.sum(y ** 2)) * np.sqrt(np.sum(u ** 2)))
+
+    for i in range(1, t):
+        y = (a - np.mean(a[:i])).flatten()
+        u = (b - np.mean(b[i:])).flatten()
+        ruy[i] = np.sum(y[:-i] * u[i:]) / (
+            np.sqrt(np.sum(y[:-i] ** 2)) * np.sqrt(np.sum(u[i:] ** 2))
+        )
+
+    return ruy
+
 class ResiduesAnalysis:
     """Residues analysis for Polynomial NARX model."""
 
