dfefd4a77e3e2d6d4981047407bb95dee1363279
Wilson Rocha <wilsonrljr@outlook.com>
2021-10-14 17:58:50 -0300

    added check_random_state function for reproducibility

--- sysidentpy/utils/_check_arrays.py
+++ sysidentpy/utils/_check_arrays.py
@@ -5,7 +5,35 @@
 
 
 import numpy as np
+import numbers
 
+# copy-pasted/adpted from scikit-learn utils/validation.py
+def check_random_state(seed):
+    """Turn `seed` into a `np.random.RandomState` instance.
+    Parameters
+    ----------
+    seed : {None, int, `numpy.random.Generator`,
+            `numpy.random.RandomState`}, optional
+        If `seed` is None (or `np.random`), the `numpy.random.RandomState`
+        singleton is used.
+        If `seed` is an int, a new ``RandomState`` instance is used,
+        seeded with `seed`.
+        If `seed` is already a ``Generator`` or ``RandomState`` instance then
+        that instance is used.
+    Returns
+    -------
+    seed : {`numpy.random.Generator`, `numpy.random.RandomState`}
+        Random number generator.
+    """
+    if seed is None or seed is np.random:
+        return np.random.mtrand._rand
+    if isinstance(seed, (numbers.Integral, np.integer)):
+        return np.random.default_rng(seed)
+    if isinstance(seed, (np.random.RandomState, np.random.Generator)):
+        return seed
+
+    raise ValueError('%r cannot be used to seed a numpy.random.RandomState'
+                     ' instance' % seed)
 
 def _num_features(X):
     return X.shape[1]
