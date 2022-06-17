65e41c43ede1005fb00b6c57e5781426f89bda1c
Wilson Rocha <wilsonrljr@outlook.com>
2022-06-17 14:19:32 -0300

    Update regressor_code to support Fourier basis functions

--- sysidentpy/utils/narmax_tools.py
+++ sysidentpy/utils/narmax_tools.py
@@ -1,6 +1,8 @@
 from ..narmax_base import GenerateRegressors
 from ._check_arrays import _num_features
 
+import numpy as np
+
 
 def regressor_code(
     *,
@@ -9,7 +11,8 @@ def regressor_code(
     xlag=2,
     ylag=2,
     model_type="NARMAX",
-    model_representation="polynomial"
+    model_representation="polynomial",
+    basis_function=None,
 ):
     if X is not None:
         n_inputs = _num_features(X)
@@ -19,6 +22,21 @@ def regressor_code(
     regressor_code = GenerateRegressors().regressor_space(
         degree, xlag, ylag, n_inputs, model_type
     )
+
+    if basis_function.__class__.__name__ != "Polynomial" and basis_function.ensemble:
+        repetition = basis_function.n * 2
+        basis_code = np.sort(
+            np.tile(regressor_code[1:, :], (repetition, 1)),
+            axis=0,
+        )
+        regressor_code = np.concatenate([regressor_code[1:], basis_code])
+    else:
+        repetition = basis_function.n * 2
+        regressor_code = np.sort(
+            np.tile(regressor_code[1:, :], (repetition, 1)),
+            axis=0,
+        )
+
     if model_representation in ("polynomial", "general_regressors"):
         return regressor_code
     elif model_representation == "neural_network":  # exclude the constant term
