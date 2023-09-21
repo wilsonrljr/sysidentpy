433a514de1e57e46b34431d16a181b0cbb35952a
Wilson Rocha <wilsonrljr@outlook.com>
2023-09-21 19:48:25 -0300

    refactoring all methods with focus on efficiency, better readability, improved docstrings, improved loops, improved conditions and many more. Besides, rewrite the notebooks to follow the new code

--- sysidentpy/multiobjective_parameter_estimation/__init__.py
+++ sysidentpy/multiobjective_parameter_estimation/__init__.py
@@ -3,3 +3,5 @@
 #           Samir Angelo Milani Martins <milani.martins@gmail.com>
 #           Wilson Rocha Lacerda Junior <wilsonrljr@outlook.com>
 # License: BSD 3 clause
+
+from .estimators import AILS
