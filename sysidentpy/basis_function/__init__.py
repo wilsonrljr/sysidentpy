19ec0f7ca5468a20667c492469de15d52a1faaee
Wilson Rocha <wilsonrljr@outlook.com>
2024-10-25 15:48:27 -0300

    add hermite, hermite normalized and laguerre basis function. Update changelog

--- sysidentpy/basis_function/__init__.py
+++ sysidentpy/basis_function/__init__.py
@@ -3,5 +3,17 @@ from ._polynomial import Polynomial
 from ._fourier import Fourier
 from ._bersntein import Bersntein
 from ._bilinear import Bilinear
+from ._laguerre import Laguerre
+from ._hermite import Hermite
+from ._hermite_normalized import HermiteNormalized
 
-__all__ = ["Bersntein", "Bilinear", "Fourier", "Legendre", "Polynomial"]
+__all__ = [
+    "Bersntein",
+    "Bilinear",
+    "Fourier",
+    "Legendre",
+    "Laguerre",
+    "Hermite",
+    "HermiteNormalized",
+    "Polynomial",
+]
