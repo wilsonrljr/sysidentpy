b8a69eb59bb3161a46294fd5246c6555d0591dc0
Wilson Rocha <wilsonrljr@outlook.com>
2025-03-04 12:40:49 -0300

    improve Least squares SignErrorLMS classes docstring

--- sysidentpy/parameter_estimation/estimators.py
+++ sysidentpy/parameter_estimation/estimators.py
@@ -623,11 +623,6 @@ class LeastMeanSquares(BaseEstimator):
         -------
         theta : array-like of shape (n_features, 1)
             The estimated parameters of the model.
-
-        Notes
-        -----
-        A more in-depth documentation of all methods for parameters estimation
-        will be available soon. For now, please refer to the mentioned references.
         """
         n_theta, n, theta, self.xi = self._initial_values(psi)
 
@@ -643,12 +638,19 @@ class LeastMeanSquares(BaseEstimator):
 
 
 class LeastMeanSquaresSignError(BaseEstimator):
-    """Least Mean Squares (LMS) filter for parameter estimation.
+    """Least Mean Squares (LMS) filter for parameter estimation using sign-error.
+
+    The sign-error LMS algorithm uses the sign of the error vector to update the filter
+    coefficients.
 
     Parameters
     ----------
     mu : float, default=0.01
         The learning rate or step size for the LMS algorithm.
+    unbiased : bool, optional
+        If True, applies an unbiased estimator. Default is False.
+    uiter : int, optional
+        Number of iterations for the unbiased estimator. Default is 30.
 
     Attributes
     ----------
@@ -662,6 +664,14 @@ class LeastMeanSquaresSignError(BaseEstimator):
     -------
     optimize(psi: np.ndarray, y: np.ndarray) -> np.ndarray
         Estimate the model parameters using the LMS filter.
+
+    References
+    ----------
+    - Hayes, M. H. (2009). Statistical digital signal processing and modeling.
+    John Wiley & Sons.
+    - Zipf, J. G. F. (2011). Classificação, análise estatística e novas estratégias de
+    algoritmos LMS de passo variável.
+    - Wikipedia entry on Least Mean Squares: https://en.wikipedia.org/wiki/Least_mean_squares_filter
     """
 
     def __init__(self, *, mu: float = 0.01, unbiased: bool = False, uiter: int = 30):
@@ -672,38 +682,34 @@ class LeastMeanSquaresSignError(BaseEstimator):
         self.xi: np.ndarray
 
     def optimize(self, psi: np.ndarray, y: np.ndarray) -> np.ndarray:
-        """Parameter estimation using the Sign-Error  Least Mean Squares filter.
+        r"""Parameter estimation using the Sign-Error Least Mean Squares filter.
 
-        The sign-error LMS algorithm uses the sign of the error vector
-        to change the filter coefficients.
+        The sign-error LMS algorithm updates the parameter estimates recursively as
+        follows:
+
+        1. Compute the estimation error:
+
+           $$
+           \xi_i = y_i - \psi_i^T \theta_{i-1}
+           $$
+
+        2. Update the parameter vector:
+
+           $$
+           \theta_i = \theta_{i-1} + \mu \cdot \text{sign}(\xi_i) \cdot \psi_i
+           $$
 
         Parameters
         ----------
         psi : ndarray of floats
             The information matrix of the model.
-        y : array-like of shape = y_training
-            The data used to training the model.
+        y : array-like of shape (n_samples, 1)
+            The data used to train the model.
 
         Returns
         -------
-        theta : array-like of shape = number_of_model_elements
+        theta : array-like of shape (n_features, 1)
             The estimated parameters of the model.
-
-        Notes
-        -----
-        A more in-depth documentation of all methods for parameters estimation
-        will be available soon. For now, please refer to the mentioned
-        references.
-
-        References
-        ----------
-        - Book: Hayes, M. H. (2009). Statistical digital signal processing
-           and modeling. John Wiley & Sons.
-        - Dissertation (Portuguese): Zipf, J. G. F. (2011). Classificação,
-           análise estatística e novas estratégias de algoritmos LMS de passo
-           variável.
-        - Wikipedia entry on Least Mean Squares
-           https://en.wikipedia.org/wiki/Least_mean_squares_filter
 
         """
         n_theta, n, theta, self.xi = self._initial_values(psi)
