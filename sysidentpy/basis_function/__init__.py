fac5cf72fce62bab385eff9a446d75be844e29f6
Wilson Rocha <wilsonrljr@outlook.com>
2025-03-20 20:53:03 -0300

    update init with new methods

--- sysidentpy/basis_function/__init__.py
+++ sysidentpy/basis_function/__init__.py
@@ -1,19 +1,19 @@
 from ._legendre import Legendre
 from ._polynomial import Polynomial
 from ._fourier import Fourier
-from ._bersntein import Bersntein
+from ._bernstein import Bernstein
 from ._bilinear import Bilinear
 from ._laguerre import Laguerre
 from ._hermite import Hermite
 from ._hermite_normalized import HermiteNormalized
 
 __all__ = [
-    "Bersntein",
+    "Bernstein",
     "Bilinear",
     "Fourier",
-    "Legendre",
-    "Laguerre",
     "Hermite",
     "HermiteNormalized",
+    "Laguerre",
+    "Legendre",
     "Polynomial",
 ]
--- sysidentpy/model_structure_selection/__init__.py
+++ sysidentpy/model_structure_selection/__init__.py
@@ -6,3 +6,4 @@ from .forward_regression_orthogonal_least_squares import FROLS
 from .accelerated_orthogonal_least_squares import AOLS
 from .meta_model_structure_selection import MetaMSS
 from .entropic_regression import ER
+from .sobolev_orthogonal_forward_regression import UOFR
