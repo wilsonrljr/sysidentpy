3abcfbe48e20e68a28784d48a296efffffea1130
Wilson Rocha <wilsonrljr@outlook.com>
2025-03-20 20:40:53 -0300

    add methods from utils in __init__ to allow users to import them directly

--- sysidentpy/utils/__init__.py
+++ sysidentpy/utils/__init__.py
@@ -7,6 +7,22 @@ from .check_arrays import (
 )
 
 from .generate_data import get_miso_data, get_siso_data
+from .display_results import results
+from sysidentpy.utils.lags import (
+    get_lag_from_regressor_code,
+    get_max_lag_from_model_code,
+    get_max_xlag,
+    get_max_ylag,
+)
+
+from sysidentpy.utils.narmax_tools import regressor_code, set_weights, train_test_split
+from sysidentpy.utils.plotting import plot_results, plot_residues_correlation
+from sysidentpy.utils.save_load import save_model, load_model
+from sysidentpy.utils.simulation import (
+    get_index_from_regressor_code,
+    list_input_regressor_code,
+    list_output_regressor_code,
+)
 
 __ALL__ = [
     "check_length",
@@ -16,4 +32,19 @@ __ALL__ = [
     "check_X_y",
     "get_miso_data",
     "get_siso_data",
+    "results",
+    "get_lag_from_regressor_code",
+    "get_max_lag_from_model_code",
+    "get_max_xlag",
+    "get_max_ylag",
+    "regressor_code",
+    "set_weights",
+    "train_test_split",
+    "plot_results",
+    "plot_residues_correlation",
+    "save_model",
+    "load_model",
+    "get_index_from_regressor_code",
+    "list_input_regressor_code",
+    "list_output_regressor_code",
 ]
